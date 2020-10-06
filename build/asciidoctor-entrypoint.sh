d145f7ea02469847ce3c44acaf29ababcf0af333
flytreeleft <flytreeleft@crazydan.org>
2020-10-06 12:59:30 +0800

    Build: update asciidoctor-entrypoint.sh

--- build/asciidoctor-entrypoint.sh
+++ build/asciidoctor-entrypoint.sh
@@ -8,12 +8,12 @@ rm -rf "${book_dist}"
 
 langs=( $(cd "${book_dir}/src" && find . -maxdepth 1 -type d | sed '/^\.$/d; s|^\./||g') )
 for lang in "${langs[@]}"; do
-    echo "  Create html for ${lang} ..."
-
     mkdir -p "${book_dist}/${lang}"
     if [ -e "${book_dir}/theme/html/icon.png" ]; then
         cp -f "${book_dir}/theme/html/icon.png" "${book_dist}/${lang}"
     fi
+
+    echo "  Create html for ${lang} ..."
     asciidoctor \
         --trace \
         --backend html5 \
@@ -23,7 +23,14 @@ for lang in "${langs[@]}"; do
         "${book_dir}/src/${lang}/book.adoc" \
     && mv "${book_dist}/${lang}/book.html" "${book_dist}/${lang}/index.html"
 
-    if [ -e "${book_dist}/${lang}/book.pdf" ]; then
-        mv "${book_dist}/${lang}/book.pdf" "${book_dist}/pdf/${lang}.pdf"
-    fi
+    # echo "  Create pdf for ${lang} ..."
+    # asciidoctor \
+    #     --trace \
+    #     --require asciidoctor-pdf \
+    #     --backend pdf \
+    #     --source-dir "${book_dir}/src/${lang}" \
+    #     --template-dir "${book_dir}/templates" \
+    #     --destination-dir "${book_dist}/${lang}" \
+    #     "${book_dir}/src/${lang}/book.adoc" \
+    # && mv "${book_dist}/${lang}/book.pdf" "${book_dist}/pdf/${lang}.pdf"
 done
