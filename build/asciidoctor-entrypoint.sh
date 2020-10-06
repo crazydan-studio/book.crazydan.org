6a282ece891a677292e53cbc64eaa79dc1000ee2
flytreeleft <flytreeleft@crazydan.org>
2020-10-06 15:50:34 +0800

    Build: support to load local html resources

--- build/asciidoctor-entrypoint.sh
+++ build/asciidoctor-entrypoint.sh
@@ -9,19 +9,16 @@ rm -rf "${book_dist}"
 langs=( $(cd "${book_dir}/src" && find . -maxdepth 1 -type d | sed '/^\.$/d; s|^\./||g') )
 for lang in "${langs[@]}"; do
     mkdir -p "${book_dist}/${lang}"
-    if [ -e "${book_dir}/theme/html/icon.png" ]; then
-        cp -f "${book_dir}/theme/html/icon.png" "${book_dist}/${lang}"
-    fi
 
     echo "  Create html for ${lang} ..."
     asciidoctor \
         --trace \
         --backend html5 \
         --source-dir "${book_dir}/src/${lang}" \
-        --template-dir "${book_dir}/templates" \
         --destination-dir "${book_dist}/${lang}" \
         "${book_dir}/src/${lang}/book.adoc" \
-    && mv "${book_dist}/${lang}/book.html" "${book_dist}/${lang}/index.html"
+    && mv "${book_dist}/${lang}/book.html" "${book_dist}/${lang}/index.html" \
+    && cp -rf "${book_dir}/src/${lang}/images" "${book_dist}/${lang}"
 
     # echo "  Create pdf for ${lang} ..."
     # asciidoctor \
